29871c28d493f386e795b021397851775d019385
roman <romaan7@gmail.com>
2019-02-07 20:25:36 +0000

    Initilization
    Signed-off-by: roman <romaan7@gmail.com>

--- Sustainable_City_Management/settings.py
+++ Sustainable_City_Management/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'Sustainable_City_Management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'RAW_DATA',
+        'USER': 'roman',
+        'PASSWORD': 'roman',
+        'HOST': '52.13.100.199',
+        'PORT': '',
     }
 }
 
