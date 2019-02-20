bba822dd6727adde2cfc7aef2e349fb7ee85eab8
gammingfreak <ashutosh07.sharma@gmail.com>
2019-02-20 03:21:20 +0000

    weather updated

--- WeatherPollution/models.py
+++ WeatherPollution/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Weather(models.Model):
-    Station = models.CharField(max_length=100)
+    Station = models.CharField(max_length=50)
     Temperature = models.IntegerField()
     Weathercol = models.CharField(max_length=25)
-    windspeed = models.CharField(max_length=5)
-    windgust = models.CharField(max_length=5)
+    windspeed = models.IntegerField()
+    windgust = models.IntegerField()
     winddirection = models.CharField(max_length=10)
     humidity = models.IntegerField()
-    rainfall = models.DecimalField(max_digits=9, decimal_places=6)
-    pressure = models.CharField(max_length=5)
+    rainfall = models.DecimalField(max_digits=9, decimal_places=3)
+    pressure = models.IntegerField()
+    last_update = models.DateTimeField('date published')
--- WeatherPollution/views.py
+++ WeatherPollution/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 import logging
 import datetime
 from WeatherPollution.models import Weather as wq
+from WeatherPollution import csvValidation
 import csv;
 import urllib.request
+import os
+from django.utils.timezone import make_aware
 # Setting up logging
 logging.basicConfig(filename='WeatherPollution/logs/view.log', level=logging.DEBUG)
 
@@ -18,20 +19,33 @@ def pull_weather_csv(csv_file):
         url = 'https://www.met.ie/latest-reports/observations/download'
         urllib.request.urlretrieve(url, csv_file)
 
-    except IOError:
+    except IOError as e:
+        print(e)
         print("IO Error in get weather CSV")
 
 
 def pushNewData():
     # print the keys and values
-    print("Tryinig------------")
-    csv_file='D:\Trinity_DS\AdvancedSoftwareEngineernig\Project\Sustainable_City_Management\WeatherPollution\CSV/weather.csv'
-    pull_weather_csv(csv_file)
-    with open(csv_file, newline='') as csvfile:
+    old_file_path = 'D:/Trinity_DS/AdvancedSoftwareEngineernig/Project/Sustainable_City_Management/WeatherPollution/CSV/weather_old.csv'
+    new_csv_file  = 'D:/Trinity_DS/AdvancedSoftwareEngineernig/Project/Sustainable_City_Management/WeatherPollution/CSV/weather_new.csv'
+    fileExists = os.path.isfile(old_file_path)
+    if fileExists == False:
+        pull_weather_csv(old_file_path)
+        writeDatatoPostgres(old_file_path)
+    else:
+        pull_weather_csv(new_csv_file)
+        if csvValidation.csv_update_validate(old_file_path,new_csv_file) == False:
+            writeDatatoPostgres(new_csv_file)
+            pull_weather_csv(old_file_path)
+        else:
+            print("No change in file so no write operation required")
+def writeDatatoPostgres(csv_path):
+    with open(csv_path, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
+        last_update = datetime.datetime.now()
         for row in reader:
-            Station=row['Station']
-            Temperature=row['Temperature (ÂºC)']
+            Station = row['Station']
+            Temperature = row['Temperature (ÂºC)']
             Weathercol = row['Weather']
             windspeed = row['Wind Speed (Kts)']
             windgust = row['Wind Gust (Kts)']
@@ -40,17 +54,25 @@ def pushNewData():
             rainfall = row['Rainfall (mm)']
             pressure=row['Pressure (hPa)']
 
-            print("########## Temperature ########## " + Temperature)
-            if humidity == '-':
-                print("True--------")
-                humidity = 999
-                print("########## humidity ##########"+humidity)
-            if Temperature == '-':
-                print("True--------")
-                Temperature = 999
-                print("########## humidity ##########" + Temperature)
-
-            Weather = wq.objects.create(Station=Station,
+            if Station == '-' or Station == 'n/a' or Station == "":
+                Station = None
+            if Temperature == '-' or Temperature == 'n/a' or Temperature == "":
+                Temperature = None
+            if Weathercol == '-' or Weathercol == 'n/a' or Weathercol == "":
+                Weathercol = None
+            if windspeed == '-' or windspeed == 'n/a' or windspeed == "":
+                windspeed = None
+            if windgust == '-'  or windgust == 'n/a' or windgust == "":
+                windgust = None
+            if winddirection == '-' or winddirection == 'n/a' or winddirection == "":
+                winddirection = None
+            if humidity == '-' or humidity == 'n/a' or humidity == "":
+                humidity = None
+            if rainfall == '-' or rainfall == 'n/a' or rainfall == "":
+                rainfall = None
+            if pressure == '-' or pressure == 'n/a' or pressure == "":
+                pressure = None
+            Weather = wq.objects.create(Station = Station,
                                     Temperature = Temperature,
                                     Weathercol = Weathercol,
                                     windspeed = windspeed,
@@ -58,7 +80,8 @@ def pushNewData():
                                     winddirection = winddirection,
                                     humidity = humidity,
                                     rainfall = rainfall,
-                                    pressure = pressure)
+                                    pressure = pressure,
+                                    last_update=last_update)
             #print(Weather)
             Weather.save()
 
