77d59b05c8004746c573af0165713a1cb2437b99
uditar <rethareu@tcd.ie>
2019-03-14 16:26:17 +0000

    Main

--- WeatherPollution/models.py
+++ WeatherPollution/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Weather(models.Model):
-    Station = models.CharField(max_length=50)
-    Temperature = models.IntegerField()
-    Weathercol = models.CharField(max_length=25)
-    windspeed = models.IntegerField()
-    windgust = models.IntegerField()
-    winddirection = models.CharField(max_length=10)
+    Station = models.CharField(max_length=50 ,null=True)
+    Temperature = models.IntegerField(null=True)
+    Weathercol = models.CharField(max_length=25 ,null=True)
+    windspeed = models.IntegerField(null=True)
+    windgust = models.IntegerField(null=True)
+    winddirection = models.CharField(max_length=10,null=True)
     humidity = models.IntegerField()
-    rainfall = models.DecimalField(max_digits=9, decimal_places=3)
-    pressure = models.IntegerField()
-    last_update = models.DateTimeField('date published')
+    rainfall = models.DecimalField(max_digits=9, decimal_places=3,null=True)
+    pressure = models.IntegerField(null=True)
+    last_update = models.DateTimeField('date published',null=True)
