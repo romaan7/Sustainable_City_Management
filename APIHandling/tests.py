54caef9e119fcf52359bdeab933824adeba5ce39
bhavesh2429 <mayekarb@tcd.ie>
2019-03-27 13:41:59 +0000

    Updated test cases

--- APIHandling/tests.py
+++ APIHandling/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
 
-# Create your tests here.
+# pytestmark = pytest.mark.django_db
+from django.test import TestCase
+from django.urls import reverse, resolve
+from datetime import datetime
+import django.utils.timezone
+
+class APIHandlingTestCase(TestCase):
+    # def test_index1(self):
+    #     resp = self.client.get('/APIHandling/')
+    #     print(resp)
+    #     self.assertEqual(resp.status_code, 200)
+
+    # Test view for BusLuas app
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/APIHandling/')
+        self.assertEquals(view.view_name, 'APIHandling:index')
