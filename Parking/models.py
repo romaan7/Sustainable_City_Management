e9d6825f580ebc1455570131c020c393be9fe4e2
bhavesh2429 <mayekarb@tcd.ie>
2019-04-06 16:31:03 +0100

    Cleaned Parking

--- Parking/models.py
+++ Parking/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
-
-class Parking(models.Model):
-    id1 = models.IntegerField()
-    location = models.CharField(max_length=100)
-    roadname = models.CharField(max_length=100)
-    noofspaces = models.IntegerField()
-    spacetype = models.CharField(max_length=100)
-    OBJECTID = models.IntegerField()
-    Point = models.CharField(max_length=100)
-    lat = models.DecimalField(max_digits=12, decimal_places=9)
-    long = models.DecimalField(max_digits=12, decimal_places=9)
-    last_update = models.DateTimeField('date published', null=True)
-    cm_last_insert_dttm = models.DateTimeField(default=timezone.now, blank=True)
+#
+# class Parking(models.Model):
+#     id1 = models.IntegerField()
+#     location = models.CharField(max_length=100)
+#     roadname = models.CharField(max_length=100)
+#     noofspaces = models.IntegerField()
+#     spacetype = models.CharField(max_length=100)
+#     OBJECTID = models.IntegerField()
+#     Point = models.CharField(max_length=100)
+#     lat = models.DecimalField(max_digits=12, decimal_places=9)
+#     long = models.DecimalField(max_digits=12, decimal_places=9)
+#     last_update = models.DateTimeField('date published', null=True)
+#     cm_last_insert_dttm = models.DateTimeField(default=timezone.now, blank=True)
 
 class carparkData(models.Model):
     name = models.CharField(max_length=50)
